9c4a8fb80a66088b5e2f1b3e28ded00d0f798210
Tony Polinelli <tonypolinelli@gmail.com>
2016-04-02 14:03:10 -0700

    React setup

---
+++ webpack.config.js
@@ -0,0 +1,35 @@
+var webpack = require('webpack');
+var path = require('path');
+var fs = require('fs');
+var loader = require('babel-loader');
+var WebpackNotifierPlugin = require('webpack-notifier');
+
+// config
+
+module.exports = {
+  entry: './app/app.js',
+  output: {
+    filename: 'public/scripts/app.js'
+  },
+  devtool: 'source-map',
+  module: {
+	  loaders: [
+      {
+        test: /\.less$/,
+        loader: 'style!css!less'
+      },
+			{
+				test: /\.js(x?)$/,
+        exclude: /(node_modules|bower_components)/,
+				loader: 'babel',
+        query: {
+          plugins: [["transform-decorators-legacy"]],
+          presets: ['react', 'es2015', 'stage-1']
+        }
+			}
+		]
+	},
+  plugins: [
+    new WebpackNotifierPlugin()
+  ]
+};
