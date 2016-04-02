9533c759e4de39767cef27283a2fbeb2c4183c82
Tony Polinelli <tonypolinelli@gmail.com>
2016-04-02 10:36:43 -0700

    mongo setup

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 var express = require('express');
 var app = express();
-var pg = require('pg');
+var MongoClient = require('mongodb').MongoClient;
 
 app.set('port', (process.env.PORT || 5000));
 
@@ -15,8 +15,37 @@ app.get('/', function(request, response) {
 });
 
 
+app.post('/db', function (request, response) {
+  response.send('this is a post');
+  // db.createCollection('test', function(err, collection) {
+  //   var doc1 = {'hello':'doc1'};
+  //   var doc2 = {'hello':'doc2'};
+  //   var lotsOfDocs = [{'hello':'doc3'}, {'hello':'doc4'}];
+  //
+  //   collection.insert(doc1);
+  //
+  //   collection.insert(doc2, {w:1}, function(err, result) {});
+  //
+  //   //collection.insert(lotsOfDocs, {w:1}, function(err, result) {});
+  // });
+});
+
 app.get('/db', function (request, response) {
   //
+
+  var conn = "mongodb://ch1:chpass@ds023398.mlab.com:23398/croudhero-pulse";
+
+  MongoClient.connect(conn, function(err, db) {
+    if(!err) {
+      console.log("We are connected");
+
+      var cursor = db.collection('test').find( );
+      cursor.toArray()
+        .then(function(result) {
+          response.render('pages/db', {result: result});
+        });
+    }
+  });
 })
 
 app.get('/test', function(request, response) {
